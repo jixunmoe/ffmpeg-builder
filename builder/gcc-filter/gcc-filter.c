4d1328e7b9990411d2270f6f9290eef4ef865638
Jixun Wu <5713045+jixunmoe@users.noreply.github.com>
2020-09-17 10:59:31 +0100

    Experiment works (#2)
    modulear build scripts
    
    * add libsdl (for ffplay)
    * add gcc7 dockerfile (untested yet)
    * remove llvm (clang) version, untested.
    * use bz2, so one can specify "snapshot" to build.

--- builder/gcc-filter/gcc-filter.c
+++ builder/gcc-filter/gcc-filter.c
@@ -2,11 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifndef PATH_TO_CC
+#define PATH_TO_CC "/usr/bin/gcc"
+#endif
+
 int main(int argc, char** argv) {
   char** my_argv = (char**)calloc(argc + 1, sizeof(char*));
 
-  // TODO: include it via macro
-  my_argv[0] = "/usr/bin/gcc-10";
+  my_argv[0] = PATH_TO_CC;
 
   int i = 1;
   int j = 1;
