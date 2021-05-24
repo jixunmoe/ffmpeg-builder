f523e57311afa51b7c2683f15240b401f1a5f56f
Jixun <Jixun.Moe@Gmail.Com>
2021-05-24 02:44:18 +0100

    update package versions; add machine specific config

---
+++ builder/make_arch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -ex
+
+# get current cpu arch:
+# cat /sys/devices/cpu/caps/pmu_name
+# gcc -march=native -Q --help=target | awk '/march/ {print $2}'
+
+for BUILD_ARCH in broadwell silvermont znver2; do
+  INPUT_FILE=Dockerfile.gcc-10
+  OUTPUT_FILE="${INPUT_FILE}.${BUILD_ARCH}"
+  sed "s/\(C\(XX\)\?FLAGS\)=\"/\1=\"-march=${BUILD_ARCH} /" < "$INPUT_FILE" > "$OUTPUT_FILE"
+done
