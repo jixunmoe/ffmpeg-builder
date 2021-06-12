0a5ce1551cd70f85e3bf671f712352f7867f2e36
Jixun Wu <5713045+jixunmoe@users.noreply.github.com>
2021-06-12 20:56:31 +0100

    Exp/GitHub ci (#3)
    Create builder and publish them in GitHub packages.
    
    * Removed gcc7 docker file
    * Updated documentation

--- builder/make_arch.sh
+++ builder/make_arch.sh
@@ -6,7 +6,7 @@ set -ex
 # cat /sys/devices/cpu/caps/pmu_name
 # gcc -march=native -Q --help=target | awk '/march/ {print $2}'
 
-for BUILD_ARCH in broadwell silvermont znver2; do
+for BUILD_ARCH in x86-64 broadwell silvermont znver2; do
   INPUT_FILE=Dockerfile.gcc-10
   OUTPUT_FILE="${INPUT_FILE}.${BUILD_ARCH}"
   sed "s/\(C\(XX\)\?FLAGS\)=\"/\1=\"-march=${BUILD_ARCH} /" < "$INPUT_FILE" > "$OUTPUT_FILE"
